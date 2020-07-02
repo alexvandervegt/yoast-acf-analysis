34e5decdfcc8228ec3566868a564029fe641ee0a
IreneStr <irene@yoast.com>
2020-07-02 15:32:59 +0200

    Set minimum YoastSEO version to 14.6

--- inc/dependencies/dependency-yoast-seo.php
+++ inc/dependencies/dependency-yoast-seo.php
@@ -10,7 +10,7 @@
  */
 final class Yoast_ACF_Analysis_Dependency_Yoast_SEO implements Yoast_ACF_Analysis_Dependency {
 
-	const MINIMAL_REQUIRED_VERSION = 14.5;
+	const MINIMAL_REQUIRED_VERSION = 14.6;
 
 	/**
 	 * Checks if this dependency is met.
